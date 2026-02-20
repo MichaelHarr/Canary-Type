807eff3cc1b82142ef94060dd8f335bcc4c44e11
MichaelHarr <michaelharrison2717@gmail.com>
2026-02-20 12:53:30 +0000

    Display levelCard component

--- frontend/app/components/levelCard.tsx
+++ frontend/app/components/levelCard.tsx
@@ -8,9 +8,11 @@ export function LevelCard({
     description: string;
 }) {
   return (
-    <div className="border rounded-lg p-4">
-      <h2 className="text-xl font-bold mb-2">{`Level ${levelNum}: ${title}`}</h2>
-      <p>{description}</p>
-    </div>
+    <a href={`/level/${levelNum}`} key={levelNum} className="block mt-4 group">
+      <div className="border rounded-lg p-4">
+          <h2 className="text-xl font-bold mb-2">{`Level ${levelNum}: ${title}`}</h2>
+          <p>{description}</p>
+      </div>
+    </a>
   );
 }
--- frontend/app/routes/level.tsx
+++ frontend/app/routes/level.tsx
@@ -1,6 +1,14 @@
 import { useParams } from "react-router";
+import type { Route } from "./+types/level";
 
-export function level() {
-    let params = useParams();
+export async function loader({ params } : Route.LoaderArgs) {
+    return {}
+}
+
+export default function level({
+    loaderData,
+}: Route.ComponentProps) {
+    let params = useParams()
+    console.log(params)
     return <h1>Level {params.levelNum}</h1>;
 }
