21513360a4ef3cdb0bb5d43860c29dcf16deed0b
MichaelHarr <michaelharrison2717@gmail.com>
2026-03-05 21:41:25 +0000

    Create LevelData type in frontend

--- frontend/app/routes/level.tsx
+++ frontend/app/routes/level.tsx
@@ -1,7 +1,13 @@
-import { useParams } from "react-router";
+import { useParams, useLoaderData } from "react-router";
 import type { Route } from "./+types/level";
 
-export async function loader({ params } : Route.LoaderArgs) {
+type LevelData = {
+    levelNum: number;
+    name: string;
+    typingText: string;
+}
+
+export async function loader({ params } : Route.LoaderArgs): Promise<LevelData> {
     
     const response = await fetch(
         `http://backend:8080/levels/byNum/${params.levelNum}`
@@ -11,21 +17,24 @@ export async function loader({ params } : Route.LoaderArgs) {
         throw new Response("Failed to fetch level data", { status: 500 });
     }
 
-    const data = await response.json();
+    const data: LevelData = await response.json();
     console.log("Response data:", data);
 
     return data;
 }
 
-export default function level({
-    loaderData,
-}: Route.ComponentProps) {
-    let params = useParams()
-    console.log(loaderData);
+export default function Level() {
+    const params = useParams();
+    const loaderData = useLoaderData() as LevelData | undefined;
+
     return (
-    <>
-        <h1>Level {params.levelNum}</h1>
-        <h2>Hello</h2>
-    </>
+        <div className="min-h-screen flex items-center j-ustify-center px-4">
+            <div className="w-full max-w-4xl text-center">
+                <h1 className="text-lg text-gray-500 mb-6">Level {loaderData?.levelNum}</h1>
+                <p className="text-4xl md:text-6xl font-semibold leading-tight">
+                    {loaderData?.typingText ?? "Loading..."}
+                </p>
+            </div>
+        </div>
     );
 }
